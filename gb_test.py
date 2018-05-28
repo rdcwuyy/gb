fef8970a58f4cfb2173c2abaaaaf6abf8b41535f
Yanying Wu <yanying.wu@gmail.com>
2018-05-28 20:22:27 +0100

    test uex.gb

--- gb_test.py
+++ gb_test.py
@@ -5,6 +5,7 @@ import urllib.request
 
 # Download GenBank file
 gbk, headers = urllib.request.urlretrieve('ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/297/395/GCF_000297395.2_ASM29739v2/GCF_000297395.2_ASM29739v2_genomic.gbff.gz')
+# gbk, headers = urllib.request.urlretrieve('file:///home/wing/workspace/gb/Sequence_Files/uex_short.gb')
 
 # Genome browser generation.
 # It creates a genome browser ready to view in a Firefox browser.
